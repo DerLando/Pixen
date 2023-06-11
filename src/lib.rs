affbb06661b92f9194b206624ac467f8e1b4706b
Lando <lando.schumpich@gmail.com>
2023-06-11 15:29:43 +0200

    Add initial mvp of pixel window

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+#![deny(clippy::all)]
+#![forbid(unsafe_code)]
+mod pixel;
+
+pub type Color = [u8; 4];
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
---
+++ src/pixel.rs
@@ -0,0 +1,53 @@
+use crate::Color;
+
+struct PixelWindow<'a> {
+    width: u32,
+    height: u32,
+    raw_buffer: &'a mut [u8],
+}
+
+impl<'a> PixelWindow<'a> {
+    pub fn new(width: u32, height: u32, raw_buffer: &'a mut [u8]) -> Self {
+        Self {
+            width,
+            height,
+            raw_buffer,
+        }
+    }
+
+    #[inline]
+    fn xy_to_index(&self, x: u32, y: u32) -> usize {
+        (y * self.width * 4 + x * 4) as usize
+    }
+
+    #[inline]
+    fn write_to_buffer(&mut self, start: usize, bytes: &[u8]) {
+        self.raw_buffer[start..=start + bytes.len()].copy_from_slice(bytes);
+    }
+
+    pub fn set_pixel(&mut self, x: u32, y: u32, color: Color) {
+        let x = x % self.width;
+        let y = y % self.height;
+
+        self.set_pixel_unchecked(x, y, color)
+    }
+
+    pub fn set_pixel_unchecked(&mut self, x: u32, y: u32, color: Color) {
+        let index = self.xy_to_index(x, y);
+        self.write_to_buffer(index, &color)
+    }
+
+    pub fn get_pixel(&self, x: u32, y: u32) -> Color {
+        let x = x % self.width;
+        let y = y % self.height;
+
+        self.get_pixel_unchecked(x, y)
+    }
+
+    pub fn get_pixel_unchecked(&self, x: u32, y: u32) -> Color {
+        let index = self.xy_to_index(x, y);
+        self.raw_buffer[index..=index + 4]
+            .try_into()
+            .expect("Buffer is big enough")
+    }
+}
